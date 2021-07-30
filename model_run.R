f2dfa01d76cd5c30d2a2396a5447bb163e6d8211
Pierre Gauzere pierre.gauzere@univ-grenoble-alpes.fr <gauzerep@f-dahu.u-ga.fr>
2021-07-30 10:13:48 +0200

    upload working OAR and model_run

--- model_run.R
+++ model_run.R
@@ -69,7 +69,7 @@ modelList <- list( typeNames = 'DA', ng = 100, burnin = 10,
 # RUN THE MODEL
 ################################################################################
 output <- gjam(formulaB, xdata, ydata, modelList)
-save(output, file='gjamOutput.RData')
+save(output, file='outputs/gjamOutput.RData')
 
 
 
